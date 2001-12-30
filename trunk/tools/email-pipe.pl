ce04463a3049c670182ba0854addf68efabc3035
Richard Soderberg <rsoderberg@gmail.com>
2001-12-30 08:39:08 -0500

    add instructions more verbose debugging

--- trunk/tools/email-pipe.pl
+++ trunk/tools/email-pipe.pl
@@ -1,6 +1,11 @@
 #!perl
 
 # $Id$
+
+# Instructions:
+#   This is a mail filter.  It can be used from .forwards, aliases,
+#   Mail::Audit, and the like.  I hard-coded the IP address; maybe
+#   I ought to parse the config problem.
 use strict;
 use LWP::UserAgent ();
 
@@ -11,10 +16,10 @@ my $url = 'http://64.53.6.49:8888/post';
 local $/;
 
 # and here we post the entirety of STDIN to memephage
-my $response = LWP::UserAgent->new->post($url, message => <>);
+my $response = LWP::UserAgent->new->post($url, {message => scalar <>});
 
 # explode if the post failed for some reason
-die "FAILURE!" unless $response->is_success;
+die $response->content unless $response->is_success;
 
 # ah, the sweet taste of success. give the shell a true return code.
 exit 0;
