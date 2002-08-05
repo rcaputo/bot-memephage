6653c0de8855b293b566ed73d70d3cda05778e67
Tod McQuillin <devin@spamcop.net>
2002-08-05 08:26:42 -0400

    load the proper database back end dynamically at run time based on the  config file

--- trunk/Util/Database.pm
+++ trunk/Util/Database.pm
@@ -12,23 +12,15 @@ use Util::Conf;
 
 use vars qw(@ISA @EXPORT);
 @ISA    = qw(Exporter);
-@EXPORT = qw( FLUSH_FIRST_MINUTES FLUSH_REST_MINUTES
-              LINK DESC USER TIME PAGE_TITLE PAGE_DESC
-              PAGE_KEYS PAGE_TIME PAGE_SIZE PAGE_TYPE
-              CHECK_TIME CHECK_STATUS MENTION_COUNT
-              REDIRECT FORA
-              get_link_id get_link_by_id get_link_obj_by_id
-	      get_recent_links get_stale_links get_unchecked_links
-	      get_links_since
-              link_set_status link_set_title link_set_meta_desc
-              link_set_meta_keys link_set_head_time link_set_head_size
-              link_set_head_type link_set_redirect
-              link_get_head_size
-            );
 
-# XXX: Use the config file here someday.
-use Database::File;
-push @EXPORT, @Database::File::EXPORT;
+my %conf  = get_items_by_name('db');
+my $database_class = "Database::\u\L$conf{type}";
+eval "use $database_class";
+die "Can't load specified database '$database_class': $@" if ($@);
+{ 
+    no strict "refs";
+    push @EXPORT, @{"${database_class}::EXPORT"};
+}
 
 #------------------------------------------------------------------------------
 1;
